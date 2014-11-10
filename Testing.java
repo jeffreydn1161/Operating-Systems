aca86576315af49eb6490ed25b2cb2b6d2251d3d
jeffreydn1161 <jeffreydn1161@gmail.com>
2014-11-09 22:25:57 -0500

    Revert "Revert "Test Code""
    This reverts commit 53014c91b5ec0f477b2fdd38e8bd8897431701f7.

--- Testing.java
+++ Testing.java
@@ -4,6 +4,9 @@ public class Testing
 {   
    public static void main(String []args)
    {
-      System.out.println("Hello World!");
+      int a = 1;
+      int b = 2;
+      int c = a+b;
+      System.out.println(a+ " "+ b+ " & "+ c);
    }
 }
